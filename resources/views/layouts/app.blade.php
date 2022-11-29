a0a9e34063feef042a596249ec261ff37141be01
Raven Barrogo <83223566+VEN-github@users.noreply.github.com>
2022-11-30 00:08:58 +0800

    ADDED: messenger chatbot

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,5 +37,36 @@
                 Have a visit at our resort at <span class="text-blue-500">55 Axis Road, Kalawaan, Pasig, Philippines</span><br />
             </footer>
         </div>
+        <!-- Messenger Chat Plugin Code -->
+        <div id="fb-root"></div>
+
+        <!-- Your Chat Plugin code -->
+        <div id="fb-customer-chat" class="fb-customerchat"></div>
+
+        <script>
+          var chatbox = document.getElementById('fb-customer-chat');
+          chatbox.setAttribute('page_id', '110887981737341');
+          chatbox.setAttribute('attribution', 'biz_inbox');
+        </script>
+
+        <!-- Your SDK code -->
+        <script>
+          window.fbAsyncInit = function () {
+            FB.init({
+              xfbml: true,
+              version: 'v15.0',
+            });
+          };
+
+          (function (d, s, id) {
+            var js,
+              fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+          })(document, 'script', 'facebook-jssdk');
+        </script>
     </body>
 </html>
