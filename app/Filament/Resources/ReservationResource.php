fc03ded7779f18c74f1b40583a365deada12ca25
jade-creator <jadelestersoriano@gmail.com>
2022-12-04 19:06:28 +0800

    changed: reserv table column name to user name

--- app/Filament/Resources/ReservationResource.php
+++ app/Filament/Resources/ReservationResource.php
@@ -36,7 +36,8 @@ class ReservationResource extends Resource
                     ->rounded()
                     ->size(150),
                 Tables\Columns\TextColumn::make('transaction_no'),
-                Tables\Columns\TextColumn::make('accommodation.name'),
+                Tables\Columns\TextColumn::make('user.last_name')
+                    ->formatStateUsing(fn (Reservation $record): string => $record->user->getFullname()),
                 Tables\Columns\TextColumn::make('package.name')->label('Event'),
                 Tables\Columns\TextColumn::make('user.email'),
                 Tables\Columns\BadgeColumn::make('status.name')
